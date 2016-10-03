b69776f35c8e302c6692d67e079f7dfcb94ea6cc
Diego Li <282108074@qq.com>
2016-10-03 16:32:39 +0800

    Add the first step of learning Servlet

---
+++ ServletDemo/src/io/happylrd/servletdemo/first/GenericServletDemoServlet.java
@@ -0,0 +1,32 @@
+package io.happylrd.servletdemo.first;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "GenericServletDemoServlet",
+        urlPatterns = {"/generic"},
+        initParams = {
+                @WebInitParam(name = "admin", value = "Diego Li"),
+                @WebInitParam(name = "email", value = "admin@example.com")
+        }
+)
+public class GenericServletDemoServlet extends GenericServlet {
+    private static final long serialVersionUID = 62500890L;
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
+        ServletConfig servletConfig = getServletConfig();
+        String admin = servletConfig.getInitParameter("admin");
+        String email = servletConfig.getInitParameter("email");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.print("<html><head></head><body>" +
+                "Admin:" + admin +
+                "<br/>Email:" + email +
+                "</body></html>");
+    }
+}
